d72abdee72f85fba2401d60155c9d0185b2b7d9d
Hugo Kaua <hugokaua2002@gmail.com>
2023-02-07 17:34:23 -0300

    commit
    adicionando event delete

--- codigo/script.js
+++ codigo/script.js
@@ -1,13 +1,27 @@
-const createBtn = document.querySelector('.create-btn');
+let task;
+const display = document.querySelector('main')
+const inputTask = document.querySelector('.input-task');
 
-createBtn.addEventListener('click', CreateNewTask);
 
-function CreateNewTask() {
-    const newTask = document.querySelector('main');
-    newTask.innerHTML += ` 
-    <section class="list" id="list">
-        <input type="checkbox" class="checkbox">
-        <input type="text" class="input-task" placeholder="Type here"></input>
-    </section>
-    `
-}
+inputTask.addEventListener('keypress', (event)=>{
+    if(event.key === 'Enter') {
+        task = inputTask.value;
+
+        const list = document.createElement('section');
+        list.classList.add('list');
+
+        const yourTask = document.createElement('input');
+        yourTask.classList.add('input-task');
+        yourTask.value = task;
+
+        display.appendChild(list).appendChild(yourTask);
+        inputTask.value = '';
+        
+        const deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('delete-btn');
+        deleteBtn.textContent = 'X';
+        list.appendChild(deleteBtn);
+
+        deleteBtn.addEventListener('click', () =>{list.remove()});
+    } 
+}); 
